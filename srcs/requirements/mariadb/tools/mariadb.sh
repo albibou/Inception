4dad1af4dd6cd3edb55ac328d0b3f2b7a644e065
albibou <alban.tardif@yahoo.fr>
2024-02-23 17:56:49 +0100

    Gogogo

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [-f /var/lib/mysql/$SQL_DATABASE]; then
+	echo "Database already exists."
+
+else
+
+	service mariadb start
+
+	sleep 5
+
+	echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" | mariadb -u root
+	echo "CREATE USER IF NOT EXISTS $SQL_USER@'localhost' IDENTIFIED BY $SQL_PASSWORD;" | mariadb -u root
+	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%' IDENTIFIED BY $SQL_PASSWORD;" | mariadb -u root
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_ROOT_PASSWORD;" | mariadb -u root
+	echo "FLUSH PRIVILEGES;" | mariadb -u root
+
+	mariadb -u root -p$SQL_ROOT_PASSWORD shutdown
+
+fi
+
+exec mysqld_safe
