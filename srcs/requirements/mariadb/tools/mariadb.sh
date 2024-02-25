fb53ed512fb6f5c1b34df6f4ad9ecaaab7b788c6
albibou <alban.tardif@yahoo.fr>
2024-02-25 21:04:21 +0100

    on y est presque

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,10 +10,10 @@ else
 	sleep 5
 
 	echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;" | mariadb -u root
-	echo "CREATE USER IF NOT EXISTS $SQL_USER@'localhost' IDENTIFIED BY $SQL_PASSWORD;" | mariadb -u root
-	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%' IDENTIFIED BY $SQL_PASSWORD;" | mariadb -u root
-	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY $SQL_ROOT_PASSWORD;" | mariadb -u root
-	echo "FLUSH PRIVILEGES;" | mariadb -u root
+	echo "CREATE USER IF NOT EXISTS $SQL_USER@'localhost' IDENTIFIED BY '$SQL_PASSWORD';" | mariadb -u root
+	echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO $SQL_USER@'%' IDENTIFIED BY '$SQL_PASSWORD';" | mariadb -u root
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';" | mariadb -u root
+	echo "FLUSH PRIVILEGES;" | mariadb -u root -p$SQL_ROOT_PASSWORD
 
 	mariadb -u root -p$SQL_ROOT_PASSWORD shutdown
 
