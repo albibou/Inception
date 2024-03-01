f60c87222db4379c95790ee2bcd682b4d4fd54cb
albibou <alban.tardif@yahoo.fr>
2024-03-01 13:43:18 +0100

    preeeesque

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -5,7 +5,7 @@ if [ -f /var/www/html/wp-config.php ]; then
 
 else
 
-	sleep 30
+	sleep 20
 
 	wp config create --allow-root \
 		--dbname=$SQL_DATABASE \
@@ -14,7 +14,10 @@ else
 		--dbhost=mariadb:3306 \
 		--path='/var/www/html'
 
-	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
+	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL --path='/var/www/html'
 	wp user create $WP_SECOND_USER $WP_SECOND_MAIL --user_pass=$WP_SECOND_PASSWD --allow-root --role=author --path='/var/www/html'
 
 fi
+
+echo "je sors du script"
+exec /usr/sbin/php-fpm7.4 -F
