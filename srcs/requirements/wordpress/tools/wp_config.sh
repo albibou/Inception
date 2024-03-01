7551ad118362d6296684fa565391595dceaed9c2
albibou <alban.tardif@yahoo.fr>
2024-03-02 00:29:39 +0100

    C'est good

--- srcs/requirements/wordpress/tools/wp_config.sh
+++ srcs/requirements/wordpress/tools/wp_config.sh
@@ -7,15 +7,17 @@ else
 
 	sleep 20
 
-	wp config create --allow-root \
+	echo "je vais installer le wordpress"
+
+	./wp-cli.phar config create --allow-root \
 		--dbname=$SQL_DATABASE \
 		--dbuser=$SQL_USER \
 		--dbpass=$SQL_PASSWORD \
 		--dbhost=mariadb:3306 \
 		--path='/var/www/html'
 
-	wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL --path='/var/www/html'
-	wp user create $WP_SECOND_USER $WP_SECOND_MAIL --user_pass=$WP_SECOND_PASSWD --allow-root --role=author --path='/var/www/html'
+	./wp-cli.phar core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL --path='/var/www/html'
+	./wp-cli.phar user create $WP_SECOND_USER $WP_SECOND_MAIL --user_pass=$WP_SECOND_PASSWD --allow-root --role=author --path='/var/www/html'
 
 fi
 
